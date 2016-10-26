b03effc5ff2c65bc7e74d2707587137caa5af892
bradyt1 <bradytoon17@outlook.com>
2016-10-26 09:56:23 -0700

    Add files via upload

---
+++ java/Asc.java
@@ -0,0 +1,14 @@
+// file: Asc.java
+// javac Asc.java
+//  java Asc
+class Asc {
+	public static void main (string a[]){
+		//variables
+		int i;// integer i 
+		char c;// character c
+		// for loop
+		for (i = 32; i < 100; i++){
+			System.out.println(i);
+		}
+	}
+}
