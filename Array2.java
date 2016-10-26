11a8956c896c0105cbc459b8a22db9f3f3c22e34
bradyt1 <bradytoon17@outlook.com>
2016-10-26 10:20:18 -0700

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+class Array2 {
+	public static void printArray(int[] iarray) {
+	   int length = iarray.length;
+	   int i;
+	    for (i = 0; i < length; i++){
+			System.out.print(iarray[i]);
+			System.out.print(" . ");
+	  }
+   }
+   
+    public static void main (String a[]){
+        int i;
+        int[] ia ;ia = new int[21];
+        
+            ia[0] = -4;
+            ia[1] = -8;
+            ia[2] = -4;
+            ia[3] = -8;
+            ia[4] = -4;
+            ia[5] = -8;
+            ia[6] = -4;
